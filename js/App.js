88c4d49b1649d702a397fdd660c0a51e6b326169
YBDmatS <sasia.mathilde@gmail.com>
2026-02-23 12:50:03 +0100

    [FEAT] Issue 2 - Factory Pattern

--- js/App.js
+++ js/App.js
@@ -1,29 +1,30 @@
 class App {
-    constructor() {
-        this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        
-        this.oldMoviesApi = new MovieApi('/data/old-movie-data.json')
-        this.newMoviesApi = new MovieApi('/data/new-movie-data.json')
-    }
+  constructor() {
+    this.$moviesWrapper = document.querySelector(".movies-wrapper");
+    this.oldMoviesApi = new MovieApi("/data/old-movie-data.json");
+    this.newMoviesApi = new MovieApi("/data/new-movie-data.json");
+    this.externalMoviesData = new MovieApi("/data/external-movie-data.json");
+  }
 
-    async main() {
-        const oldMoviesData = await this.oldMoviesApi.getMovies()
-        const newMoviesData = await this.newMoviesApi.getMovies()
+  async main() {
+    const newMoviesData = await this.newMoviesApi.getMovies();
+    const externalMoviesData = await this.externalMoviesData.getMovies();
 
-        const OldMovies = oldMoviesData
-            .map(movie => new MoviesFactory(movie, 'oldApi'))
-        const NewMovies = newMoviesData.map(movie => new MoviesFactory(movie, 'newApi'))
+    const newMovies = newMoviesData.map(
+      (movie) => new MoviesFactory(movie, "newApi"),
+    );
+    const externalMovie = externalMoviesData.map(
+      (movie) => new MoviesFactory(movie, "externalApi"),
+    );
 
-        const FullMovies = OldMovies.concat(NewMovies)
-        
-        FullMovies.forEach(movie => {
-                const Template = new MovieCard(movie)
-                this.$moviesWrapper.appendChild(
-                    Template.createMovieCard()
-                )
-            })
-    }
+    const fullMovies = [...externalMovie, ...newMovies];
+
+    fullMovies.forEach((movie) => {
+      const Template = new MovieCard(movie);
+      this.$moviesWrapper.appendChild(Template.createMovieCard());
+    });
+  }
 }
 
-const app = new App()
-app.main()
+const app = new App();
+app.main();
--- js/factories/MoviesFactory.js
+++ js/factories/MoviesFactory.js
@@ -1,14 +1,13 @@
 class MoviesFactory {
-    constructor(data, type) {
-        // Si le type correspond à l'ancienne API, alors retourne moi l'ancien formattage
-        if (type === 'oldApi') {
-            return new OldMovie(data)
-        // Sinon retourne moi le nouveau formattage
-        } else if (type === 'newApi') {
-            return new Movie(data)
-        // Une bonne pratique est de throw une erreur si le format n'est pas reconnu
-        } else {
-            throw 'Unknown type format'
-        }
+  constructor(data, type) {
+    if (type === "oldApi") {
+      return new OldMovie(data);
+    } else if (type === "newApi") {
+      return new Movie(data);
+    } else if (type === "externalApi") {
+      return new ExternalMovie(data);
+    } else {
+      throw new Error("Type de factory inconnu");
     }
+  }
 }
