c6614a63b4a23722b5da3b63a01fc2eee1527697
YBDmatS <sasia.mathilde@gmail.com>
2026-02-23 12:35:18 +0100

    [FEAT] Issue 1 - Conctructor Pattern

--- js/App.js
+++ js/App.js
@@ -1,24 +1,19 @@
 class App {
-    constructor() {
-        this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/old-movie-data.json')
-    }
+  constructor() {
+    this.$moviesWrapper = document.querySelector(".movies-wrapper");
+    this.moviesApi = new MovieApi("/data/new-movie-data.json");
+  }
 
-    async main() {
-        // Ici je récupère mes films de mon fichier old-movie-data.json
-        const moviesData = await this.moviesApi.getMovies()
-        
-        moviesData
-            // Ici, je transforme mon tableau de données en un tableau de classe Movie
-            .map(movie => new OldMovie(movie))
-            .forEach(movie => {
-                const Template = new MovieCard(movie)
-                this.$moviesWrapper.appendChild(
-                    Template.createMovieCard()
-                )
-            })
-    }
+  async main() {
+    const moviesData = await this.moviesApi.getMovies();
+    const movies = moviesData.map((movie) => new Movie(movie));
+    console.log(movies);
+    movies.forEach((movie) => {
+      const Template = new MovieCard(movie);
+      this.$moviesWrapper.appendChild(Template.createMovieCard());
+    });
+  }
 }
 
-const app = new App()
-app.main()
+const app = new App();
+app.main();
--- js/models/Movie.js
+++ js/models/Movie.js
@@ -1,3 +1,36 @@
 class Movie {
+  constructor(data) {
+    this._title = data.title;
+    this._synopsis = data.synopsis;
+    this._picture = data.picture;
+    this._duration = data.duration;
+    this._released_in = data.released_in;
+  }
 
+  get title() {
+    if (typeof this._title.fr != "undefined") {
+      return this._title.fr;
+    } else if (typeof this._title.en != "undefined") {
+      return this._title.en;
+    } else {
+      return "";
+    }
+  }
+
+  get synopsis() {
+    return this._synopsis;
+  }
+
+  get picture() {
+    return this._picture;
+  }
+
+  get duration() {
+    const formatDuration = `${Math.floor(this._duration / 60)}h${this._duration % 60}`;
+    return formatDuration;
+  }
+
+  get released_in() {
+    return this._released_in;
+  }
 }
