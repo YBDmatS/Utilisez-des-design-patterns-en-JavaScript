d6d58859d010808454542381fe9116b3b3b1f3a5
YBDmatS <sasia.mathilde@gmail.com>
2026-02-23 14:47:24 +0100

    [FEAT] Issue 4 - Singleton Pattern

--- js/db/User.js
+++ js/db/User.js
@@ -1,3 +1,43 @@
 class User {
-    
+  constructor(data) {
+    if (User.exists) {
+      return User.instance;
+    } else if (data && data.firstName && data.lastName) {
+      this._firstName = data.firstName;
+      this._lastName = data.lastName;
+
+      User.instance = this;
+      User.exists = true;
+
+      this.saveUserToLocalStorage();
+      return this;
+    }
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  get user() {
+    const firstName = this._firstName || localStorage.getItem("firstName");
+    const lastName = this._lastName || localStorage.getItem("lastName");
+
+    if (!firstName || !lastName) {
+      return null;
+    }
+
+    return {
+      firstName: firstName,
+      lastName: lastName,
+    };
+  }
+
+  saveUserToLocalStorage() {
+    localStorage.setItem("firstName", this.firstName);
+    localStorage.setItem("lastName", this.lastName);
+  }
 }
