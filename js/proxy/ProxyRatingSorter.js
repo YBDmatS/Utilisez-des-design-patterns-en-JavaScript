da5ad3d072a3a95d49f00a3521a22d2e8ff444fd
YBDmatS <sasia.mathilde@gmail.com>
2026-02-24 13:33:10 +0100

    [FEAT] Issue 10 - Proxy Pattern

--- js/proxy/ProxyRatingSorter.js
+++ js/proxy/ProxyRatingSorter.js
@@ -1,8 +1,19 @@
 class ProxyRatingSorter {
-    constructor() {
-        this.cache = []
+  constructor() {
+    this.cache = [];
+  }
+
+  async sorter(data, orderBy) {
+    const cachedResult = this.cache.find((item) => item.key === orderBy);
+
+    if (cachedResult) {
+      console.log("Get from cache");
+      return cachedResult;
     }
 
-    async sorter() {
-    }
+    return RatingSorterApi.sorter(data, orderBy).then((result) => {
+      this.cache.push({ key: orderBy, data: result.data });
+      return result;
+    });
+  }
 }
--- js/templates/SorterForm.js
+++ js/templates/SorterForm.js
@@ -1,50 +1,51 @@
 class SorterForm {
-    constructor(Movies) {
-        this.Movies = Movies
+  constructor(Movies) {
+    this.Movies = Movies;
 
-        this.$wrapper = document.createElement('div')
-        this.$sorterFormWrapper = document.querySelector('.sorter-form-wrapper')
-        this.$moviesWrapper = document.querySelector('.movies-wrapper')
+    this.$wrapper = document.createElement("div");
+    this.$sorterFormWrapper = document.querySelector(".sorter-form-wrapper");
+    this.$moviesWrapper = document.querySelector(".movies-wrapper");
 
-        this.ProxyRatingSorter = new ProxyRatingSorter()
+    this.ProxyRatingSorter = new ProxyRatingSorter();
+  }
+
+  async sorterMovies(sorter) {
+    this.clearMoviesWrapper();
+
+    if (!!sorter) {
+      // Vous pourrez supprimer cette ligne
+      const sortedData = await this.ProxyRatingSorter.sorter(
+        this.Movies,
+        sorter,
+      );
+
+      const SortedMovies = sortedData.data;
+
+      SortedMovies.forEach((Movie) => {
+        const Template = new MovieCard(Movie);
+        this.$moviesWrapper.appendChild(Template.createMovieCard());
+      });
+    } else {
+      this.Movies.forEach((Movie) => {
+        const Template = new MovieCard(Movie);
+        this.$moviesWrapper.appendChild(Template.createMovieCard());
+      });
     }
+  }
 
-    async sorterMovies(sorter) {
-        this.clearMoviesWrapper()
+  onChangeSorter() {
+    this.$wrapper.querySelector("form").addEventListener("change", (e) => {
+      const sorter = e.target.value;
+      this.sorterMovies(sorter);
+    });
+  }
 
-        if (!!sorter) {
-            // Vous pourrez supprimer cette ligne
-            const sortedData = await RatingSorterApi.sorter(this.Movies, sorter)
+  clearMoviesWrapper() {
+    this.$moviesWrapper.innerHTML = "";
+  }
 
-            const SortedMovies = sortedData.data 
-
-            SortedMovies.forEach(Movie => {
-                const Template = new MovieCard(Movie)
-                this.$moviesWrapper.appendChild(Template.createMovieCard())
-            })
-        } else {
-            this.Movies.forEach(Movie => {
-                const Template = new MovieCard(Movie)
-                this.$moviesWrapper.appendChild(Template.createMovieCard())
-            })
-        }
-    }
-
-    onChangeSorter() {
-        this.$wrapper
-            .querySelector('form')
-            .addEventListener('change', e => {
-                const sorter = e.target.value
-                this.sorterMovies(sorter)
-            })
-    }
-
-    clearMoviesWrapper() {
-        this.$moviesWrapper.innerHTML = ""
-    }
-
-    render() {
-        const sorterForm = `
+  render() {
+    const sorterForm = `
             <form action="#" method="POST" class="sorter-form">
                 <label for="sorter-select">Triez par date de sortie : </label>
                 <select name="sorter-select" id="sorter-select">
@@ -53,11 +54,11 @@ class SorterForm {
                     <option value="DESC">Décroissante</option>
                 </select>
             </form>
-        `
+        `;
 
-        this.$wrapper.innerHTML = sorterForm
-        this.onChangeSorter()
+    this.$wrapper.innerHTML = sorterForm;
+    this.onChangeSorter();
 
-        this.$sorterFormWrapper.appendChild(this.$wrapper)
-    }
+    this.$sorterFormWrapper.appendChild(this.$wrapper);
+  }
 }
